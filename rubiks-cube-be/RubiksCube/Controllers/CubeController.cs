dbe23da2da00f3d708ca2549ef804f9c03f61705
Iva Petkova <iva1petkova@gmail.com>
2025-04-01 02:14:36 +0300

    refactor

--- rubiks-cube-be/RubiksCube/Controllers/CubeController.cs
+++ rubiks-cube-be/RubiksCube/Controllers/CubeController.cs
@@ -1,19 +1,16 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using RubiksCube.Models;
 using RubiksCube.Services;
 
 namespace RubiksCube.Controllers;
 
 [Route("[controller]")]
-public class CubeController(ILogger<CubeController> logger) : ControllerBase
+public class CubeController() : ControllerBase
 {
 
     [HttpPost("rotate")]
     public IActionResult Rotate([FromBody] CubeRequestModel model)
     {
-        //  logger.LogInformation(JsonConvert.SerializeObject(model));
         RotationService.Rotate(model);
         return Ok(model.Cube);
     }
--- rubiks-cube-be/RubiksCube/Services/RotationService.cs
+++ rubiks-cube-be/RubiksCube/Services/RotationService.cs
@@ -9,7 +9,6 @@ namespace RubiksCube.Services
     {
         public static void Rotate(CubeRequestModel model)
         {
-
             var cube = model.Cube;
             Dictionary<Face, Colour[][]> sides = cube.Sides;
             string cubeSerialized = JsonConvert.SerializeObject(cube);
@@ -22,24 +21,13 @@ namespace RubiksCube.Services
         private static void RotateSide(Move direction, Dictionary<Face, Colour[][]> sides, Dictionary<Face, Colour[][]> sidesCopy)
         {
             IndexMove[][] moves = RotationUtilities.GetSideMoves(direction);
-
+            bool isPrime = direction.ToString().Contains("Prime", StringComparison.OrdinalIgnoreCase);
             foreach (IndexMove[] moveGroup in moves)
             {
                 foreach (IndexMove move in moveGroup)
                 {
-                    Tile from;
-                    Tile to;
-
-                    if (direction.ToString().Contains("Prime", StringComparison.OrdinalIgnoreCase))
-                    {
-                        from = move.To;
-                        to = move.From;
-                    }
-                    else
-                    {
-                        from = move.From;
-                        to = move.To;
-                    }
+                    Tile from = isPrime ? move.To : move.From;
+                    Tile to = isPrime ? move.From : move.To;
 
                     Colour colour = sidesCopy[from.Face][from.Position[0]][from.Position[1]];
                     sides[to.Face][to.Position[0]][to.Position[1]] = colour;
@@ -47,7 +35,5 @@ namespace RubiksCube.Services
                 }
             }
         }
-
-
     }
 }
