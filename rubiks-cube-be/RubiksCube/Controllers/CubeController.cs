1d0f720e29161f1edd6252c498348e3f9ec3cd52
Iva Petkova <iva1petkova@gmail.com>
2025-04-02 01:15:15 +0300

    Add log message after each rotation

--- rubiks-cube-be/RubiksCube/Controllers/CubeController.cs
+++ rubiks-cube-be/RubiksCube/Controllers/CubeController.cs
@@ -1,17 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using RubiksCube.Models;
 using RubiksCube.Services;
 
 namespace RubiksCube.Controllers;
 
 [Route("[controller]")]
-public class CubeController() : ControllerBase
+public class CubeController(ILogger<CubeController> logger) : ControllerBase
 {
 
     [HttpPost("rotate")]
     public IActionResult Rotate([FromBody] CubeRotationModel model)
     {
         RotationService.Rotate(model);
+        string logMessage = $"rotatedCube: {JsonConvert.SerializeObject(model)}";
+        logger.LogInformation(logMessage);
         return Ok(model.Cube);
     }
 
